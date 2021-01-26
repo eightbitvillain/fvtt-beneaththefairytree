0c372f23828dedf31eb53b2d2545e980bdb661df
eightbit <8bitvoid@gmail.com>
2021-01-26 14:57:24 -0600

    Stripped to bare bones

--- module/sheets/btft-actor-sheet.js
+++
@@ -1,6 +0,0 @@
-export default class BTFTActorSheet extends ActorSheet {
-  get template() {
-    console.log("BeyondTheFairyTree | Actor Sheet Loaded")
-    return `systems/beyondthefairytree/templates/sheets/btft-actor-sheet-${this.actor.data.type}.html`
-  }
-}
--- module/sheets/btft-item-sheet.js
+++
@@ -1,5 +0,0 @@
-export default class BTFTItemSheet extends ItemSheet {
-  get template() {
-    return `systems/beyondthefairytree/templates/sheets/btft-${this.item.data.type}-sheet.html`
-  }
-}
